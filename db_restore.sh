b5bc5875c7811aefbf25e0d9c743957eeb575516
Steve Zaske <github@zaske.com>
2017-07-21 16:58:39 -0700

    minor script fix

--- db_restore.sh
+++ db_restore.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 echo "Creating database 'wildlife_tracker', database: Postgres"
+psql -c "DROP DATABASE wildlife_tracker;"
+psql -c "DROP DATABASE wildlife_tracker_test;"
 psql -c "CREATE DATABASE wildlife_tracker;"
 psql wildlife_tracker < wildlife_tracker_db_backup.sql
 psql -c "CREATE DATABASE wildlife_tracker_test WITH TEMPLATE wildlife_tracker;"
